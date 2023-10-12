a7b824181a4fe8d216bc5912e4f4744bc23e4786
Christoph Fink <christoph.fink@gmail.com>
2023-10-12 16:24:11 +0300

    debug statements

--- src/retooter/retooter.py
+++ src/retooter/retooter.py
@@ -123,11 +123,16 @@ class Retooter:
 
     @functools.cached_property
     def account_name(self):
+        print("XXXXX")
+        print(os.environ[ACCOUNT_NAME])
+        print(len(os.environ[ACCOUNT_NAME].strip().split("@")))
         try:
             account_name = os.environ[ACCOUNT_NAME].strip()
             print(account_name, len(account_name.split("@")))
             assert account_name
-            assert (len(account_name.split("@")) == 2)
+            assert (
+                len(account_name.split("@")) == 2
+            ), f"Include the instance domain in {ACCOUNT_NAME}"
         except (AssertionError, KeyError) as exception:
             raise RetooterNoAccountNameDefined(
                 f"{ACCOUNT_NAME} is not set or invalid, check configuration"

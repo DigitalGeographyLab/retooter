25dfce8de90c58e5a0393c9768b20fb2d83be2fa
Christoph Fink <christoph.fink@gmail.com>
2024-01-12 10:28:16 +0200

    (debugging) print every mention, before filtering

--- src/retooter/retooter.py
+++ src/retooter/retooter.py
@@ -180,6 +180,10 @@ class Retooter:
                     mention["status"]["id"]
                 )
             ]
+            print(
+                f"Mentioned in post {mention['status']['id']}"
+                f"by {mention['status']['account']['acct']}"
+            )
             if (
                 self.account_name not in retooters
                 and self.account_name.split("@")[0] not in retooters
